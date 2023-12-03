b4b0750105f71a14cf731615c2d88f49f0c0598d
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2023-12-03 16:54:57 +0300

    Results yapılandırması yapıldı.

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,13 @@ namespace Business.Abstract
 {
     public interface ICarService
     {
-        List<Car> GetAll();
-        List<CarDetailDTO> GetCarDetail();
-        List<Car> GetByIdBrand(int brandId);
-        void Add(Car car);
-        void Update(Car car);
-        void Delete(Car car);
+        IDataResult<List<Car>> GetAll();
+        IDataResult<List<CarDetailDTO>> GetCarDetail();
+        IDataResult<List<Car>> GetByIdBrand(int brandId);
+        IDataResult<Car> GetById(int id);
+        IResult Add(Car car);
+        IResult Update(Car car);
+        IResult Delete(Car car);
 
     }
 }
--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
@@ -19,29 +21,32 @@ namespace Business.Concrete
             _colorDal = colorDal;
         }
 
-        public void Add(Color color)
+        public IResult Add(Color color)
         {
             _colorDal.Add(color);
+            return new SuccesResult(Messages.ColorAdded);
         }
 
-        public void Delete(Color color)
+        public IResult Delete(Color color)
         {
             _colorDal.Delete(color);
+            return new SuccesResult(Messages.ColorDeleted);
         }
 
-        public List<Color> GetAll()
+        public IDataResult<List<Color>> GetAll()
         {
-            return _colorDal.GetAll();
+            return new SuccessDataResult<List<Color>>(_colorDal.GetAll(), Messages.ColorListed);
         }
 
-        public Color GetByColor(int colorId)
+        public IDataResult<Color> GetByColor(int colorId)
         {
-           return _colorDal.Get(c=>c.Id==colorId);   
+           return new SuccessDataResult<Color>(_colorDal.Get(c=>c.Id==colorId));   
         }
 
-        public void Update(Color color)
+        public IResult Update(Color color)
         {
             _colorDal.Update(color);
+            return new SuccesResult(Messages.ColorUpdated);
         }
     }
 }
