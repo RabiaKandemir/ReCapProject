e81f96e3bd46105def972294f4e46bd8619588c2
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2023-12-04 02:41:58 +0300

    Autofac eklendi,Fluent Validation eklendi,AOP alt yapısı kuruldu

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,9 @@
 
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependecyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -18,18 +21,12 @@ namespace WebAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<ICarService, CarManager>();
-            builder.Services.AddSingleton<ICarDal, EfCarDal>();
-            builder.Services.AddSingleton<IBrandService, BrandManager>();
-            builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
-            builder.Services.AddSingleton<IColorService, ColorManager>();
-            builder.Services.AddSingleton<IColorDal, EFColorDal>();
-            builder.Services.AddSingleton<IUserService, UserManager>();
-            builder.Services.AddSingleton<IUserDal, EfUserDal>();
-            builder.Services.AddSingleton<ICustomerService, CustomerManager>();
-            builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
-            builder.Services.AddSingleton<IRentalService, RentalManager>();
-            builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+            builder.Host
+       .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+       .ConfigureContainer<ContainerBuilder>(builder =>
+       {
+           builder.RegisterModule(new AutofacBusinessModule());
+       });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
